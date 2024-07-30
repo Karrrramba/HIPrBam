c12ba279e99aba725b15d76d6d913408e83c8f1a
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-07-30 16:56:41 +0200

    Added summarized output

--- code/modeling.R
+++ code/modeling.R
@@ -1,11 +1,15 @@
 library(mgcv)
+library(future)
 
 # Model functions----
 fit_null_model <- function(data){
+  
+  future::plan(multisession)
+  
   lambda <- 10^-12
   K = max(as.numeric(data$fraction))
   
-  with(data, gam(relative_intensity ~ s(fraction, k = K, sp = lambda),
+  with(data, mgcv::gam(relative_intensity ~ s(fraction, k = K, sp = lambda),
                  data = data,
                  method = "REML",
                  family = "gaussian",
@@ -16,7 +20,7 @@ fit_alt_model <- function(data){
   lambda <- 10^-12
   K = max(as.numeric(data$fraction))
   
-  with(data, gam(relative_intensity ~ s(fraction, by = treatment, k = K, sp = lambda),
+  with(data, mgcv::gam(relative_intensity ~ s(fraction, by = treatment, k = K, sp = lambda),
                  data = data,
                  method = "REML",
                  family = "gaussian",
@@ -28,45 +32,57 @@ fit_models <- function(data) {
   fit_null <- fit_null_model(data)
   fit_alt <- fit_alt_model(data)
   
-  null_predicted <- fit_null[[3]]
-  null_residuals <- fit_null[[2]]
-  alt_predicted <- fit_alt[[3]]
-  alt_residuals <- fit_alt[[2]]
+  null_predicted <- fit_null$fitted.values
+  # null_residuals <- fit_null$residuals
+  null_df <- fit_null$df.residual #70 - df.residual ?
+  null_conv <- fit_null$converged
+  # alt_predicted <- fit_alt$fitted.values
+  alt_residuals <- fit_alt$residuals
+  alt_df <- fit_alt$df.residual #70 - df.residual ?
+  alt_conv <- fit_alt$converged
   
-  # Output and summarize combined parameters in a table
   output <- data %>% 
     mutate(
-      null_predicted = null_predicted,
-      null_residuals = null_residuals,
-      alt_predicted = alt_predicted,
-      alt_residuals = alt_residuals
-      # p_value = p_val
+      # predicted_null = null_predicted,
+      residuals_null = null_residuals,
+      # predicted_alt = alt_predicted,
+      residuals_alt = alt_residuals,
+      df_null = null_df,
+      df_alt = alt_df,
+      converged_null = null_conv,
+      converged_alt = alt_conv
     )
   
   return(output)
 }
 
+
+
 # wrap model summary into function
 # extract model parameters for each gene and save into environment
-
-model_summary <- model_details %>% 
-  select(gene_name, mends_with("residuals")) %>% 
-  group_by(gene_name) %>% 
-  summarise(across(ends_with("residuals"), ~sum(.x^2))) %>% 
-  ungroup()
 
 # Apply model fitting to complete data set
 set.seed(123)
 data_modeled <- data_averaged %>% 
   group_by(gene_name) %>% 
   do(fit_models(.)) %>% 
-  ungroup() %>% 
-  mutate(fdr = p.adjust(p_value, method = "BH"))
+  ungroup() 
+
+model_summary <- data_modeled %>% 
+  select(gene_name, starts_with("residuals"), df_null:converged_alt) %>% 
+  group_by(gene_name) %>% 
+  summarise(across(starts_with("residuals"), ~sum(.x^2)),
+            across(starts_with("df"), mean),
+            across(starts_with("converged"), mean),
+            .groups = "drop") %>%
+  rename_with(~ gsub("residuals", "rss", .x, fixed = TRUE)) %>% 
+  mutate(
+    across(starts_with("rss"), ~round(.x, 2)),
+    delta_rss = round(rss_null - rss_alt, 2),
+    across(starts_with("converged"), ~if_else(.x == 1, TRUE, FALSE))
+    )
+
 
 # Apply Benjamini-Hochberg correction for multiple testing
-# significant_hits <- 
-  data_modeled %>% 
-  filter(FDR <= 0.05) %>% 
-  left_join(proteome_data %>% select(c(protein_id, protein_name, gene_name), by = gene_name))
   
 # Identify fractions with greatest differences based on relative intensity
